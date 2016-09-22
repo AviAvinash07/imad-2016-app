e67bc0a7d6427d4c6190c3766f7e05000b2b9c98
AviAvinash07 <AviAvinash07@users.noreply.github.com>
2016-09-22 11:33:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,13 @@ element.innerHTML = "New Value";
 
 var img = document.getElementById("madi");
 
+var marginLeft = 0;
+
+function moveRight(){
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + "px";
+}
+
 madi.onclick = function(){
-    
-    madi.style.marginLeft="100px";
+    var interval = setInterval(moveRight, 100);    
 };
